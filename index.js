59a65f96de60dedc64b9deecef4e332bb0fd1f08
DKay-code <derya.kaygisiz@outlook.com>
2021-05-21 19:17:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+  return  sourceArray.map (x => x * -1);
+}
+
+function mapToNoChange(sourceArray) {
+    return  sourceArray.map (x => x);
+  }
+
+function mapToDouble(sourceArray) {
+    return  sourceArray.map (x => x * 2);
+  }
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(x => x * x);
+}
+  
+function reduceToTotal(sourceArray){ 
+    return  sourceArray.reduce((accumulator, current) => accumulator + current);
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){ 
+    return  sourceArray.reduce(((accumulator, current) => accumulator + current), startingPoint);
+}
+
+function reduceToAllTrue(sourceArray){ 
+    return  sourceArray.reduce((accumulator, current) => {
+        if (current) {
+            return true
+        } else {
+            return false;
+          }
+    });
+}
+
+function reduceToAnyTrue(sourceArray){ 
+    return  sourceArray.reduce((accumulator, current) => {
+        if (current) {
+            return true
+        } else {
+            return false;
+          }
+    });
+}
